881941a50533397d24fc37d2071c7888ac432c59
Daniel Oom <daoo314@gmail.com>
2014-02-24 14:00:39 +0100

    Add more functionallity

---
+++ src/Music/Theory/Diatonic.hs
@@ -0,0 +1,20 @@
+module Music.Theory.Diatonic
+  ( Diatonic(..)
+  ) where
+
+-- |Diatonic Intervals.
+data Diatonic
+  = PerfectUnison
+  | Minor2
+  | Major2
+  | Minor3
+  | Major3
+  | Perfect4
+  | Tritone
+  | Perfect5
+  | Minor6
+  | Major6
+  | Minor7
+  | Major7
+  | PerfectOctave
+  deriving (Show, Eq, Ord, Enum)
---
+++ src/Music/Theory/Rooted.hs
@@ -0,0 +1,15 @@
+module Music.Theory.Rooted
+  ( Rooted(..)
+  , hasNote
+  ) where
+
+import Music.Theory.Note
+import Music.Theory.Scale
+
+data Rooted = Rooted
+  { root :: Note
+  , scale :: Scale
+  } deriving Show
+
+hasNote :: Note -> Rooted -> Bool
+hasNote n (Rooted r s) = hasOffset s (scaled $ n `off` r)
